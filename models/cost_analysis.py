a81c6506b884c9888d0c520dfebf49867e011f0c
K.M. Azizullah Tanim <tanim.aziz913@gmail.com>
2023-02-07 16:08:56 +0600

    adding more feilds and computing total cost and unit price

--- models/cost_analysis.py
+++ models/cost_analysis.py
@@ -10,4 +10,32 @@ class CostAnalysis(models.Model):
     _rec_name = 'product_id'
 
     product_id = fields.Many2one("product.product", string="Product Name")
+    partner_id = fields.Many2one("res.partner", string="Partner Name")
     line_ids = fields.One2many('costing.line', 'cost_analysis_id', string="Cost")
+    lc_number = fields.Char(string="LC Number")
+    name = fields.Char(string="Name")
+    date = fields.Datetime(string="Date")
+    scheduled_deadline = fields.Datetime(string="Scheduled Deadline")
+    product_qty = fields.Integer(string="Product Quantity")
+    lc_price = fields.Float(string= "LC Price")  
+    bank_number = fields.Char(string="Bank Number")
+    total_cost = fields.Float(string= "Total Cost", compute='_get_calculate_total_cost')  
+    unit_cost = fields.Float(string= "Unit Cost", compute='_get_calculate_unit_cost')  
+
+
+    @api.depends('line_ids')
+    def _get_calculate_total_cost(self):
+        total = 0
+        for obj in self.line_ids:
+            total += obj.cost
+        self.total_cost = total
+    
+    @api.depends('line_ids','product_qty')
+    def _get_calculate_unit_cost(self):
+        total = 0
+        for obj in self.line_ids:
+            total += obj.cost 
+        if self.product_qty != 0:
+            self.unit_cost = total / self.product_qty
+        else:
+            self.unit_cost = 0
